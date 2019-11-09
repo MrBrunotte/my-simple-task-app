360ec1fabf4cdcddf9fd3da29c2324baa92e0c83
mrbrunotte <mrbrunotte@gmail.com>
2019-11-09 11:11:38 +0100

    Add base and modify tasks

--- app.py
+++ app.py
@@ -1,13 +1,13 @@
 import os
 from flask import Flask, render_template, redirect, request, url_for
 from flask_pymongo import PyMongo
-from bson.objectid import ObjectId 
+from bson.objectid import ObjectId
 
 app = Flask(__name__)
 
 app.config['MONGO_DBNAME'] = 'task_manager'
-app.config['MONGO_URI'] = os.getenv('MONGO_URI', 'mongodb://localhost')
-#app.config['MONGO_URI'] = 'mongodb+srv://mrbrunotte:mrUSERbrunotte@foodictionary-gckbp.mongodb.net/task_manager?retryWrites=true&w=majority'
+#app.config['MONGO_URI'] = os.getenv('MONGO_URI', 'mongodb://localhost')
+app.config['MONGO_URI'] = 'mongodb+srv://mrbrunotte:mrUSERbrunotte@foodictionary-gckbp.mongodb.net/task_manager?retryWrites=true&w=majority'
 # Insert password and change test to db name (task_manager) change this to environment variables!!
 
 # add instance of pymongo
