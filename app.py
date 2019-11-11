96fdb5646209778c73bbc08b545d51d472e8ae7d
mrbrunotte <mrbrunotte@gmail.com>
2019-11-11 22:11:30 +0100

    Finished task manager

--- app.py
+++ app.py
@@ -41,11 +41,72 @@ def insert_task():
 # wire up the edit button, we use the ObjectId from the bson.objectid library
 @app.route('/edit_task/<task_id>')
 def edit_task(task_id):
-    the_task = mongo.db.tasks.find_one({"_id": ObjectId(task_id)}) # we look for one task and we search by "id" in the "ObjectId" (red) in the DB
-    all_categories = mongo.db.categories.find() 
+    # we look for one task and we search by "id" in the "ObjectId" (red) in the DB
+    the_task = mongo.db.tasks.find_one({"_id": ObjectId(task_id)})
+    all_categories = mongo.db.categories.find()
     # create a list of the collections so that we can find the task that we want to edit with all its categories
     return render_template('edittask.html', task=the_task,
                            categories=all_categories)
+
+# Update Task In The Database
+@app.route('/update_task/<task_id>', methods=["POST"])
+def update_task(task_id):   # we pass in the task ID because thats our hook into the primary key
+    tasks = mongo.db.tasks  # we access the tasks collection
+    tasks.update({'_id': ObjectId(task_id)},   # we call the update function, we specify the id as our key
+                 {
+        'task_name': request.form.get('task_name'),
+        'category_name': request.form.get('category_name'),
+        'task_description': request.form.get('task_description'),
+        'due_date': request.form.get('due_date'),
+        'is_urgent': request.form.get('is_urgent')
+    })
+    return redirect(url_for('get_tasks'))
+
+# Delete Task
+@app.route('/delete_task/<task_id>')
+def delete_task(task_id):
+    mongo.db.tasks.remove({'_id': ObjectId(task_id)}
+                          )  # use the remove function
+    # redirect to get_tasks to see that it is removed!
+    return redirect(url_for('get_tasks'))
+
+# Display Categories
+@app.route('/get_categories')
+def get_categories():
+    return render_template('categories.html', categories=mongo.db.categories.find())
+
+
+# we pass in the category ID into the function "edit_category()"
+@app.route('/edit_category/<category_id>')
+def edit_category(category_id):
+    return render_template('editcategory.html',
+                           category=mongo.db.categories.find_one({'_id': ObjectId(category_id)}))
+
+# Update Category In The Database
+# <category_id> targets the correct document in the DB
+@app.route('/update_category/<category_id>', methods=['POST'])
+def update_category(category_id):
+    mongo.db.categories.update(
+        # identify the ID and the field (category_name) that we want to update
+        {'_id': ObjectId(category_id)},
+        {'category_name': request.form.get('category_name')})
+    # return to the categories with the get_categories function
+    return redirect(url_for('get_categories'))
+
+
+# Add Category pt2. This function inserts the category the function below that lets us add a new category
+# no ID since it doesnt exist yet!!
+@app.route('/insert_category', methods=['POST'])
+def insert_category():
+    category_doc = {'category_name': request.form.get('category_name')}
+    # add the category_doc into our colleciton (categories table)
+    mongo.db.categories.insert_one(category_doc)
+    return redirect(url_for('get_categories'))
+
+# Add Category pt1 this lets us add a new category
+@app.route('/add_category')     
+def add_category():
+    return render_template('addcategory.html') # directs us to the addcategory page with the add_category function
 
 
 if __name__ == '__main__':
